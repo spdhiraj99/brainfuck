eb2f621fe24d27862e69407ec123e52465ab6f34
Dhiraj Sharma S <spdhiraj99@gmail.com>
2017-11-08 15:32:46 +0530

    Add files via upload

---
+++ linux/mine.h
@@ -0,0 +1,26 @@
+//exit code 1 for file opening error
+//exit code 2 for memory allocation error
+//exit code 3 for wrong extension
+//exit code 5 syntax error
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<cstdlib>
+#include<algorithm>
+#define MAX 100
+using namespace std;
+struct STACK{
+    int data;
+    struct STACK *prev;
+};
+extern int arr[MAX],r,*ptr,init;
+extern char lang[MAX];
+typedef struct STACK Stack;
+extern Stack *top,*head,*top1;
+void push(int);
+void pop();
+int peek();
+void reader(char ch);
+int checker();
+void loader();
+void runner();
